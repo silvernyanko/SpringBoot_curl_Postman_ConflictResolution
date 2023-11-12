768efa27ed4475b8170854d7976d2cbc20e7eacc
silvernyanko <h8octf.jay.mcbz@gmail.com>
2023-11-12 18:11:22 +0900

    Hello, World!

---
+++ src/main/java/com/example/greeting/GreetController.java
@@ -0,0 +1,14 @@
+package com.example.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class GreetController {
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello, World!";
+    }
+}
